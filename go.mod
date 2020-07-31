47b8756cb69366c466d6d9c4790e52fe4bb1ce5c
王开钧 <kaijun.wang@weimob.com>
2020-07-31 17:23:43 +0800

    SetUpGoMaxProcs

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/michael-kj/utils
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v8 v8.0.0-beta.7
 	github.com/jinzhu/gorm v1.9.15
 	github.com/kirinlabs/HttpRequest v1.0.5
+	go.uber.org/automaxprocs v1.3.0
 	go.uber.org/zap v1.15.0
 )
--- utils.go
+++ utils.go
@@ -2,8 +2,13 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/kirinlabs/HttpRequest"
+	"github.com/michael-kj/utils/log"
+	"go.uber.org/automaxprocs/maxprocs"
 )
 
 func Post(url string, request, response interface{}, header map[string]string) error {
@@ -27,5 +32,32 @@ func Post(url string, request, response interface{}, header map[string]string) e
 		return err
 	}
 	return nil
+
+}
+
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		method := c.Request.Method
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token")
+		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
+		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+		c.Header("Access-Control-Allow-Credentials", "true")
+
+		if method == "OPTIONS" {
+			c.AbortWithStatus(http.StatusNoContent)
+		}
+		c.Next()
+	}
+}
+func maxprocsLog(format string, v ...interface{}) {
+	log.Logger.Info(fmt.Sprintf(format, v))
+
+}
+
+func SetUpGoMaxProcs() {
+
+	maxprocs.Set(maxprocs.Logger(maxprocsLog))
 
 }
